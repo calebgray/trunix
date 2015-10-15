b7841928ccc093cf24f6221a63a935b567f68fdc
Caleb Gray <hey@calebgray.com>
2015-10-15 00:10:34 +0000

    Minor improvements.

--- src/trunix/bin/adventure.c
+++ src/trunix/bin/adventure.c
@@ -1,34 +1,29 @@
-#!/bin/cinit
+//#!/bin/cinit
 
 #include <ncurses.h>
-
-#define HOOK_INIT_INT 0
-#define HOOK_TICK_INT 1
-void *HOOK_INIT = (void *) HOOK_INIT_INT;
-void *HOOK_TICK = (void *) HOOK_TICK_INT;
 
 int tile_x, tile_y;
 
 int player_x, player_y;
-void *PlayerController(void *action) {
-  switch ((int) action) {
-  case (HOOK_INIT_INT):
-    player_x = tile_x;
-    player_y = tile_y;
-    return (void *) 'X';
-    break;
-  case (HOOK_TICK_INT):
-    return (void *) 'x';
-  default:
-    return 0;
-  }
+void *PlayerControllerInit() {
+  player_x = tile_x;
+  player_y = tile_y;
+  return (void *) 'X';
+}
+void *PlayerControllerTick() {
+ return (void *) 'x';
 }
 
 struct {
-  char ch;
-  void *(*hook)(void *);
+  char tile;
+  void *(*init)();
+  void *(*tick)();
 } TILES_SPECIAL[] = {
-  { 'X', PlayerController }
+  {
+    'X',
+    PlayerControllerInit,
+    PlayerControllerTick,
+  }
 };
 #define TILES_SPECIAL_MAX sizeof(TILES_SPECIAL) / sizeof(TILES_SPECIAL[0])
 
@@ -40,7 +35,7 @@ struct {
 };
 #define TILES_ANIM_MAX sizeof(TILES_ANIM) / sizeof(TILES_ANIM[0])
 
-struct {
+struct LEVEL {
   unsigned int width;
   unsigned int height;
   char *tiles;
@@ -87,17 +82,20 @@ struct {
 };
 #define LEVEL_MAX sizeof(LEVELS) / sizeof(LEVELS[0])
 
+LEVEL level;
+
 void level_draw(unsigned int level, bool load) {
   move(0, 0);
   for (tile_y = 0; tile_y < LEVELS[level].height; ++tile_y) {
     for (tile_x = 0; tile_x < LEVELS[level].width; ++tile_x) {
       char ch = LEVELS[level].tiles[tile_x + (tile_y * LEVELS[level].width)];
       for (int i = 0; i < TILES_SPECIAL_MAX; ++i) {
-        if (ch == TILES_SPECIAL[i].ch) {
+        if (ch == TILES_SPECIAL[i].tile) {
           if (load) {
-            ch = (int) TILES_SPECIAL[i].hook(HOOK_INIT);
+            //special_tiles.push(TILES_SPECIAL[i]);
+            ch = (int) TILES_SPECIAL[i].init();
           } else {
-            ch = (int) TILES_SPECIAL[i].hook(HOOK_TICK);
+            ch = (int) TILES_SPECIAL[i].tick();
           }
         }
       }
@@ -114,11 +112,13 @@ int main() {
   //int y = getmaxy(main);
   keypad(win, true);
   start_color();
+  
   level_draw(0, true);
   for (int i = 0; i < 10; ++i) {
     // TODO: implement player control hook
     level_draw(0, false);
   }
+  
   keypad(win, false);
   echo();
   nocbreak();
