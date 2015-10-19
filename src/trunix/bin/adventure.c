9ff805cd3d3c13db2c78e931f04e34dedf0214cc
Caleb Gray <hey@calebgray.com>
2015-10-19 00:40:47 +0000

    Game play!

--- src/trunix/bin/adventure.c
+++ src/trunix/bin/adventure.c
@@ -13,26 +13,37 @@ void level_draw();
 
 int input;
 
+int win_width, win_height;
 int tile_x, tile_y;
 
 struct {
   int width, height;
-  int specialCount, portalsCount;
   struct {
     int frame;
     char *frames;
     void (*collision)();
   } *tiles;
+  
+  int specialCount;
   struct {
     void (*tick)();
   } *specials;
+  
+  int portalsCount;
   struct {
     int x, y;
     int level;
     int destination;
   } *portals;
+  
+  int enemyCount;
+  struct {
+    int frame;
+    char *frames;
+    int x, y;
+  } *enemies;
 } gLevel = {
-  0, 0, 0, NULL, NULL
+  0, 0, NULL, 0, NULL, 0, NULL, 0, NULL
 };
 
 struct {
@@ -43,13 +54,13 @@ struct {
   {
     60, 10,
     "+----------------------------------------------------------+" \
+    "|             |                                            |" \
+    "|           A      X                                       |" \
+    "|             |                                            |" \
+    "|        -----|                                            |" \
     "|                                                          |" \
-    "| A                                                        |" \
     "|                                                          |" \
-    "|                                                          |" \
-    "|                                                          |" \
-    "|                                                          |" \
-    "|                                                        X ]" \
+    "|                                                          ]" \
     "|                                                          |" \
     "+----------------------------------------------------------+",
   },
@@ -110,12 +121,46 @@ struct {
 #define PORTAL_MAX sizeof(PORTALS) / sizeof(PORTALS[0])
 
 int player_x, player_y;
+
+enum CollisionType {
+  COLLISION_NONE,
+  COLLISION_WALL,
+  COLLISION_PLAYER,
+  COLLISION_ENEMY,
+};
+enum CollisionType CheckCollision(int x, int y, enum CollisionType ignore) {
+  int tile = x + y * gLevel.width;
+  if (ignore != COLLISION_WALL && gLevel.tiles[tile].frames[gLevel.tiles[tile].frame-1] != ' ') {
+    return COLLISION_WALL;
+  } else if (ignore != COLLISION_PLAYER && x == player_x && y == player_y) {
+    return COLLISION_PLAYER;
+  } else {
+    // enemies, etc
+  }
+  return COLLISION_NONE;
+}
+
+int player_health = 0, player_health_max = 4;
 char PlayerControllerInit() {
   player_x = tile_x;
   player_y = tile_y;
+  if (player_health == 0) {
+    player_health = player_health_max;
+  }
   return ' ';
 }
 void PlayerControllerTick() {
+  int healthbar_size = 2 + player_health_max;
+  mvaddch(1, win_width - healthbar_size - 1, '[');
+  for (int health = 1; health < player_health_max + 1; ++health) {
+    if (health <= player_health) {
+      addch('=');
+    } else {
+      addch(' ');
+    }
+  }
+  addch(']');
+  
   int oldPlayerX = player_x, oldPlayerY = player_y;
   
   switch(input) {
@@ -140,12 +185,72 @@ void PlayerControllerTick() {
     // player didn't move
   } else if (gLevel.tiles[tile].collision != NULL) {
     gLevel.tiles[tile].collision(player_x, player_y);
-  } else if (gLevel.tiles[tile].frames[gLevel.tiles[tile].frame-1] != ' ') {
+  } else if (CheckCollision(player_x, player_y, COLLISION_PLAYER) != COLLISION_NONE) {
     player_x = oldPlayerX;
     player_y = oldPlayerY;
   }
 
   mvaddch(player_y, player_x, 'x');
+}
+void PlayerHealth(int delta) {
+  player_health += delta;
+  if (player_health <= 0) {
+    // reset_game();
+    level_load(0);
+  }
+}
+
+int enemy = 0;
+int EnemyInit() {
+  enemy = gLevel.enemyCount++;
+  gLevel.enemies = realloc(gLevel.enemies, sizeof(gLevel.enemies[0]) * gLevel.enemyCount);
+  gLevel.enemies[enemy].x = tile_x;
+  gLevel.enemies[enemy].y = tile_y;
+  return enemy;
+}
+
+void EnemyTick() {
+  mvaddch(gLevel.enemies[enemy].y, gLevel.enemies[enemy].x, gLevel.enemies[enemy].frames[gLevel.enemies[enemy].frame]);
+  if (gLevel.enemies[enemy].frames[++gLevel.enemies[enemy].frame] == 0) {
+    gLevel.enemies[enemy].frame = 0;
+  }
+  if (++enemy == gLevel.enemyCount) enemy = 0;
+}
+
+char EnemyChaserInit() {
+  enemy = EnemyInit();
+  gLevel.enemies[enemy].frame = 0;
+  gLevel.enemies[enemy].frames = "Aa";
+  return ' ';
+}
+
+void EnemyChaserTick() {
+  // dumb chase
+  int oldX = gLevel.enemies[enemy].x, oldY = gLevel.enemies[enemy].y;
+  if (gLevel.enemies[enemy].x < player_x) {
+    ++gLevel.enemies[enemy].x;
+  } else if (gLevel.enemies[enemy].x > player_x) {
+    --gLevel.enemies[enemy].x;
+  } else if (gLevel.enemies[enemy].y < player_y) {
+    ++gLevel.enemies[enemy].y;
+  } else if (gLevel.enemies[enemy].y > player_y) {
+    --gLevel.enemies[enemy].y;
+  }
+  
+  enum CollisionType collision = CheckCollision(gLevel.enemies[enemy].x, gLevel.enemies[enemy].y, COLLISION_NONE);
+  if (collision != COLLISION_NONE) {
+    if (collision == COLLISION_PLAYER) {
+      PlayerHealth(-1);
+    }
+    gLevel.enemies[enemy].x = oldX;
+    gLevel.enemies[enemy].y = oldY;
+  }
+  
+  EnemyTick();
+}
+
+void EnemyChaserCollision(int x, int y) {
+  //
 }
 
 int portal;
@@ -182,6 +287,12 @@ struct {
     NULL,
   },
   {
+    'A',
+    EnemyChaserInit,
+    EnemyChaserTick,
+    EnemyChaserCollision,
+  },
+  {
     '[',
     PortalInit,
     NULL,
@@ -216,7 +327,7 @@ struct {
 };
 #define TILES_ANIM_MAX sizeof(TILES_ANIM) / sizeof(TILES_ANIM[0])
 
-void level_unload() {
+void level_unload(bool full) {
   if (gLevel.tiles != NULL) {
     int tile = 0;
     for (tile_y = 0; tile_y < gLevel.height; ++tile_y) {
@@ -225,24 +336,24 @@ void level_unload() {
         ++tile;
       }
     }
-    free(gLevel.tiles);
   }
-  if (gLevel.specials != NULL) free(gLevel.specials);
-  if (gLevel.portals != NULL) free(gLevel.portals);
+  if (full) {
+    free(gLevel.tiles);
+    free(gLevel.portals);
+    free(gLevel.enemies);
+  }
 }
 
 void level_load(unsigned int level) {
-  level_unload();
+  level_unload(false);
 
   int tile = 0;
   gLevel.width = LEVELS[level].width;
   gLevel.height = LEVELS[level].height;
-  gLevel.tiles = malloc(sizeof(gLevel.tiles[0]) * gLevel.width * gLevel.height);
+  gLevel.tiles = realloc(gLevel.tiles, sizeof(gLevel.tiles[0]) * gLevel.width * gLevel.height);
   gLevel.specialCount = 0;
-  gLevel.specials = NULL;
-  
+
   gLevel.portalsCount = 0;
-  gLevel.portals = NULL;
   for (portal = 0; portal < PORTAL_MAX; ++portal) {
     if (PORTALS[portal].from == level) {
       gLevel.portals = realloc(gLevel.portals, sizeof(gLevel.portals[0]) * (gLevel.portalsCount+1));
@@ -252,6 +363,9 @@ void level_load(unsigned int level) {
     }
   }
   portal = 0;
+  
+  gLevel.enemyCount = 0;
+  enemy = 0;
   
   for (tile_y = 0; tile_y < gLevel.height; ++tile_y) {
     for (tile_x = 0; tile_x < gLevel.width; ++tile_x) {
@@ -338,8 +452,8 @@ int main() {
   start_color();
   //
   
-  //int x = getmaxx(win);
-  //int y = getmaxy(win);
+  win_width = getmaxx(win);
+  win_height = getmaxy(win);
   
   level_load(0);
   while (input != 'q') {
@@ -350,12 +464,13 @@ int main() {
     level_draw();
     //if (diff < gNanoSpeed) usleep(gNanoSpeed - diff);
   }
-  level_unload();
+  level_unload(true);
   
   keypad(win, false);
   curs_set(1);
   echo();
   noraw();
   endwin();
+  
   return 0;
 }
